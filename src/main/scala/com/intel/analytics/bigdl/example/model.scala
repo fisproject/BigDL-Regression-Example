125221c55fad514ffd75bd2b95a9eef46520a3bb
ttsy <japan.t.egawa@gmail.com>
2017-06-16 23:31:09 +0900

    first commit

---
+++ src/main/scala/com/intel/analytics/bigdl/example/model.scala
@@ -0,0 +1,17 @@
+package com.intel.analytics.bigdl.example
+
+import com.intel.analytics.bigdl._ // Module type
+import com.intel.analytics.bigdl.nn._
+
+object SimpleRegression {
+  import com.intel.analytics.bigdl.numeric.NumericFloat
+  def apply(): Module[Float] = {
+    val model = Sequential()
+    model.add(Reshape(Array(4))) // feature size
+    .add(Linear(4, 8).setName("func_1"))
+    .add(ReLU(true))
+    .add(Linear(8, 1).setName("func_2"))
+    .add(Mean())
+    model
+  }
+}
